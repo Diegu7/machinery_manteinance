400d902e44aa22ee873b5fdcc68a8ad96398155e
Wragdan <davidfernandomendez@gmail.com>
2018-05-16 22:03:11 -0600

    corrected previous mistake, comma missing

--- db/migrate/20170606162621_create_inventory_transaction_details.rb
+++ db/migrate/20170606162621_create_inventory_transaction_details.rb
@@ -3,7 +3,7 @@ class CreateInventoryTransactionDetails < ActiveRecord::Migration[5.1]
     create_table :inventory_transaction_details do |t|
       t.integer :quantity
 
-      t.references :inventory_transaction, index: {name: 'index_invent_trans_details_invent_trans_id'} foreign_key: true
+      t.references :inventory_transaction, index: {name: 'index_invent_trans_details_invent_trans_id'}, foreign_key: true
       t.references :product, foreign_key: true
 
       t.timestamps
