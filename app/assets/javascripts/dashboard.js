3222e2f873b6621505ef58cd8f10885815a47584
Wragdan <davidfernandomendez@gmail.com>
2018-05-18 04:59:04 -0600

    small change to test deploy

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
             center: "title",
             right: "month,agendaWeek,agendaDay"
         },
-        events: "/events"
+        events:[
+            {
+                title  : 'event1',
+                start  : '2018-05-01'
+            },
+            {
+                title  : 'event2',
+                start  : '2010-01-05',
+                end    : '2010-01-07'
+            },
+            {
+                title  : 'event3',
+                start  : '2010-01-09 12:30:00',
+                allDay : false
+            }
+        ] 
     });
 });
