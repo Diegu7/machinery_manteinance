0b13efdc9d29762cb101b1b9b0e97bbd3086fa4f
Wragdan <davidfernandomendez@gmail.com>
2018-05-18 05:09:17 -0600

    changed git ignore to accept lock files, secured docker login in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker login --username=_ --password="$HEROKU_TOKEN" registry.heroku.com
+#!/bin/bash
+echo "$HEROKU_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin registry.heroku.com
 docker build -t registry.heroku.com/machinery-maintenance/web:latest .
 docker push registry.heroku.com/machinery-maintenance/web:latest
