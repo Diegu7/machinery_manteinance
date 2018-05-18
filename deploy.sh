cf4855f3f9dca2171276b4eb337e0da6f583e4a2
Wragdan <davidfernandomendez@gmail.com>
2018-05-18 05:21:51 -0600

    changed heroku docker credentials error

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$HEROKU_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin registry.heroku.com
+echo "$HEROKU_TOKEN" | docker login -u "$HEROKU_TOKEN" --password-stdin registry.heroku.com
 docker build -t registry.heroku.com/machinery-maintenance/web:latest .
 docker push registry.heroku.com/machinery-maintenance/web:latest
