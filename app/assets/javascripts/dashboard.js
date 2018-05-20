8dcea724616f647b3fdf05b1181892b9d0386b57
Wragdan <davidfernandomendez@gmail.com>
2018-05-20 07:54:27 -0600

    before events

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,28 +3,13 @@
 //= require fullcalendar/dist/locale/es
 
 $(document).ready(function() {
-    $("#calendar").fullCalendar({
-        locale: "es",
-        header: {
-            left: "prev,next today",
-            center: "title",
-            right: "month,agendaWeek,agendaDay"
-        },
-        events:[
-            {
-                title  : 'event1',
-                start  : '2018-05-01'
-            },
-            {
-                title  : 'event2',
-                start  : '2010-01-05',
-                end    : '2010-01-07'
-            },
-            {
-                title  : 'event3',
-                start  : '2010-01-09 12:30:00',
-                allDay : false
-            }
-        ] 
-    });
+  $('#calendar').fullCalendar({
+    locale: 'es',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    events: '/events'
+  });
 });
